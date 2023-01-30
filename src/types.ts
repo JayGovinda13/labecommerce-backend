c604a16eecd407545c6c49b29e40564d58f1c05e
Juliano Amaral <jaygovinda13@gmail.com>
2023-01-30 11:02:32 -0300

    atualizações feitas

--- src/types.ts
+++ src/types.ts
@@ -1,29 +1,29 @@
 //cliente cadastrado
 export type TClient = {
-    id: string
-    email: string
-    password: string
-}
+  id: string;
+  email: string;
+  password: string;
+};
 
 //produto cadastrado
 export type TProduct = {
-    id: string
-    name: string
-    price: number
-    category: string
-}
+  id: string;
+  name: string;
+  price: number;
+  category: string;
+};
 
 //compra do cliente
 export type TPurchase = {
-    userId: string
-    productId: string
-    quantity: number
-    totalPrice: number
-}
+  userId: string;
+  productId: string;
+  quantity: number;
+  totalPrice: number;
+};
 
 //categoria do produto
 export enum CATEGORY_PROD {
-    BOOKS = "Livros",
-    COMPUTERS = "Computadores",
-    CLASS = "Aulas",
+  BOOKS = "Livros",
+  COMPUTERS = "Computadores",
+  CLASS = "Aulas",
 }
